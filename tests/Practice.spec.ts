a1883cc688940d639394da247df3d9d1037e285a
Saikumar <kondajisaikumar.marolix@gmail.com>
2026-05-08 09:39:34 +0530

    practice file

--- tests/Practice.spec.ts
+++ tests/Practice.spec.ts
@@ -1,10 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('login page', async ({ page }) => {
+test('SauceDemo Login Test', async ({ page }) => {
 
-  await page.goto("https://www.saucedemo.com/")
+  // Open SauceDemo website
+  await page.goto('https://www.saucedemo.com/');
 
+  // Enter Username
+  await page.locator('//input[@id="user-name"]').fill('standard_user');
+  await page.waitForTimeout(3000);
+  // Enter Password
+  await page.locator('//input[@id="password"]').fill('secret_sauce');
+  await page.waitForTimeout(3000);
 
+ // await page.pause()
 
+  // Click Login button
+  await page.locator('//input[@id="login-button"]').click();
+  await page.waitForTimeout(3000);
   
-})
+  // Wait to see result
+  await page.waitForTimeout(3000);
+
+});
