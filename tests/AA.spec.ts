f0d5e533e1b4f766f7f0d5494bc322a4bf65c4a6
Saikumar <saitrainer123@gmail.com>
2026-09-15 12:21:33 +0530

    Update

---
+++ tests/AA.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '@playwright/test';
+
+test('JavaScript Alerts', async ({ page }) => {
+
+    // // Open website
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+})
